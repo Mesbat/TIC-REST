6405d9fcc4d6679c811eed466b4d1c1f973b7cb6
Yacine Mesbat <mesbat_y@etna-alternance.net>
2018-03-26 22:00:34 +0200

    etape2

--- app/controllers/domains.ts
+++ app/controllers/domains.ts
@@ -14,7 +14,7 @@ class DomainsController {
             .find({ select: ["id", "slug", "name", "description"] })
         };
       } catch (err) {
-          return { code: 500, message: "internal server error", datas: err };
+        return { code: 500, message: "internal server error", datas: err };
       }
     else return { code: 400, message: "bad request", datas: [] };
   }
@@ -22,17 +22,27 @@ class DomainsController {
   public async show(request: Request, response: Response) {
     if (request.params.format === "json")
       try {
+        let queryResult = await getManager()
+          .createQueryBuilder(Domain, 'domain')
+          .leftJoin('domain.creator', 'creator')
+          .leftJoin('domain.langs', 'langs')
+          .where('domain.name = :name')
+          .setParameter('name', request.params.name)
+          .select(['langs.code', 'domain.id', 'domain.slug', 'domain.name', 'domain.description', 'creator.id', 'creator.username', 'domain.created_at'])
+          .getMany();
+
         return {
           code: 200,
           message: "success",
-          datas: await getManager()
-            .createQueryBuilder(Domain, 'domain')
-            .leftJoin('domain.creator', 'domain')
-            .leftJoin('domain.langs', 'lang')
-            .where('domain.name = :name')
-            .setParameter('name', request.params.name)
-            .select(['langs.code', 'domain.id', 'domain.slug', 'domain.name', 'domain.description', 'creator.id', 'creator.username', 'domain.created_at'])
-            .getMany()
+          datas: {
+            langs: queryResult[0].langs.map((lang) => { return lang.code }),
+            id: queryResult[0].id,
+            slug: queryResult[0].slug,
+            name: queryResult[0].name,
+            description: queryResult[0].description,
+            creator: queryResult[0].creator,
+            created_at: queryResult[0].created_at.toISOString()
+          }
         };
       } catch (err) {
         return { code: 500, message: "internal server error", datas: err };
--- app/models/domain.ts
+++ app/models/domain.ts
@@ -4,17 +4,6 @@ import { User } from "./user";
 
 @Entity()
 export class Domain {
-  @PrimaryGeneratedColumn() id!: number;
-
-  @Index({ unique: true })
-  @Column("varchar") slug!: string;
-
-  @Column("varchar") name!: string;
-
-  @Column("longtext") description!: string;
-
-  @Column("datetime") created_at!: string;
-
   @ManyToMany(type => Lang)
   @JoinTable({
     name: "domain_lang",
@@ -29,8 +18,19 @@ export class Domain {
   })
   langs!: Lang[];
 
+  @PrimaryGeneratedColumn() id!: number;
+
+  @Index({ unique: true })
+  @Column("varchar") slug!: string;
+
+  @Column("varchar") name!: string;
+
+  @Column("longtext") description!: string;
+
   @Index()
   @ManyToOne(type => User)
   @JoinColumn({name: 'user_id'})
   creator!: User
+
+  @Column("datetime") created_at!: Date;
 }
