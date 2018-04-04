a06297037d621e3831f3348f8fbde762293a1d67
Yacine <mesbat_y@etna-alternance.net>
2018-04-04 10:18:08 +0200

    beautified

--- app/controllers/translations.ts
+++ app/controllers/translations.ts
@@ -1,4 +1,3 @@
-
 import { Request, Response } from "express";
 import { getManager } from "typeorm";
 import { Domain } from "../models/domain";
@@ -8,100 +7,121 @@ import { TranslationToLang } from "../models/translation-to-lang";
 import Auth from "../services/auth";
 
 interface showTranslationFormat {
-    id: number,
-    code: string,
-    trans: any
-};
+  id: number;
+  code: string;
+  trans: any;
+}
 
 class TranslationsController {
-    public async index(request: Request, response: Response) {
-        if (request.params.format === "json") {
-            if (!(await getManager().getRepository(Domain).count({ name: request.params.name })))
-                return ({ code: 404, message: "not found" });
+  public async index(request: Request, response: Response) {
+    if (request.params.format === "json") {
+      if (
+        !await getManager()
+          .getRepository(Domain)
+          .count({ name: request.params.name })
+      )
+        return { code: 404, message: "not found" };
 
-            let queryResult = await getManager()
-                .createQueryBuilder(Translation, "translation")
-                .leftJoin("translation.domain", "domain")
-                .leftJoin("domain.langs", "langs")
-                .leftJoin("translation.translatedValues", "translatedValues")
-                .leftJoin("translatedValues.lang", "lang")
-                .where("domain.name = :name")
-                .setParameter("name", request.params.name)
-                .select([
-                    "translation.id",
-                    "translation.code",
-                    "translatedValues.trans",
-                    "lang.code",
-                    "langs.code"
-                ])
-                .getMany();
+      let queryResult = await getManager()
+        .createQueryBuilder(Translation, "translation")
+        .leftJoin("translation.domain", "domain")
+        .leftJoin("domain.langs", "langs")
+        .leftJoin("translation.translatedValues", "translatedValues")
+        .leftJoin("translatedValues.lang", "lang")
+        .where("domain.name = :name")
+        .setParameter("name", request.params.name)
+        .select([
+          "translation.id",
+          "translation.code",
+          "translatedValues.trans",
+          "lang.code",
+          "langs.code"
+        ])
+        .getMany();
 
-            return {
-                code: 200,
-                message: "success",
-                datas: this.showTranslationFormatter(queryResult)
-            };
-        } else return { code: 400, message: "bad request", datas: [] };
+      return {
+        code: 200,
+        message: "success",
+        datas: this.showTranslationFormatter(queryResult)
+      };
+    } else return { code: 400, message: "bad request", datas: [] };
+  }
+
+  public async create(request: Request, response: Response) {
+    let auth = await Auth.domainAuth(request);
+
+    if (request.params.format !== "json")
+      return { code: 400, message: "bad request", datas: [] };
+
+    if (!(auth instanceof Domain)) return auth;
+
+    if (request.body.code.length < 2)
+      return {
+        code: 400,
+        message: "bad request",
+        datas: `translation code is too short`
+      };
+
+    for (var lang in request.body.trans) {
+      let unregisteredLang: any = lang;
+
+      auth.langs.forEach(registeredLang => {
+        if (registeredLang.code === lang) unregisteredLang = false;
+      });
+
+      if (unregisteredLang)
+        return {
+          code: 400,
+          message: "bad request",
+          datas: `[${unregisteredLang}] is not a registered language`
+        };
     }
 
-    public async create(request: Request, response: Response) {
-        let auth = await Auth.domainAuth(request);
+    let translation = new Translation();
+    translation.code = request.body.code;
+    translation.domain = auth;
+    translation.translatedValues = auth.langs.map(lang => {
+      let translatedValue = new TranslationToLang();
+      translatedValue.lang = lang;
+      translatedValue.translation = translation;
+      translatedValue.trans =
+        (request.body.trans && request.body.trans[lang.code]) ||
+        translation.code;
 
-        if (!(auth instanceof Domain))
-            return (auth);
+      return translatedValue;
+    });
 
-        if (request.body.code.length < 2)
-            return ({ code: 400, message: "bad request", datas: `translation code is too short` });
+    translation = await getManager().save(translation);
 
-        for (var lang in request.body.trans) {
-            let unregisteredLang: any = lang;
+    return {
+      code: 201,
+      message: "success",
+      datas: this.showTranslationFormatter([translation])[0]
+    };
+  }
 
-            auth.langs.forEach(registeredLang => {
-                if (registeredLang.code === lang)
-                    unregisteredLang = false;
-            });
+  private showTranslationFormatter(queryResult: Translation[]) {
+    let jsonResponse = queryResult.map(translation => {
+      let row: showTranslationFormat = {
+        trans: {},
+        id: translation.id,
+        code: translation.code
+      };
 
-            if (unregisteredLang)
-                return ({ code: 400, message: "bad request", datas: `[${unregisteredLang}] is not a registered language` });
-        }
+      translation.domain.langs.forEach(lang => {
+        let translatedValue = translation.translatedValues.find(
+          x => x.lang.code === lang.code
+        );
+        row.trans[lang.code] = translatedValue
+          ? translatedValue.trans
+          : translation.code;
+      });
 
-        let translation = new Translation();
-        translation.code = request.body.code;
-        translation.domain = auth;
-        translation.translatedValues = auth.langs.map(lang => {
-            let translatedValue = new TranslationToLang();
-            translatedValue.lang = lang;
-            translatedValue.translation = translation;
-            translatedValue.trans = (request.body.trans && request.body.trans[lang.code]) || translation.code;
+      return row;
+    });
 
-            return (translatedValue);
-        });
-
-        translation = await getManager().save(translation);
-
-        if (request.params.format === "json") {
-            return {
-                code: 201,
-                message: "success",
-                datas: this.showTranslationFormatter([translation])[0]
-            }
-        } else return { code: 400, message: "bad request", datas: [] };
-    }
-
-    private showTranslationFormatter(queryResult: Translation[]) {
-        let jsonResponse = queryResult.map(translation => {
-            let row: showTranslationFormat = { trans: {}, id: translation.id, code: translation.code }
-
-            translation.domain.langs.forEach(lang => {
-                let translatedValue = translation.translatedValues.find(x => x.lang.code === lang.code);
-                row.trans[lang.code] = translatedValue ? translatedValue.trans : translation.code;
-            });
-
-            return (row);
-        });
-
-        return (jsonResponse);
-    }
+    return jsonResponse;
+  }
 }
 
 export default new TranslationsController();
